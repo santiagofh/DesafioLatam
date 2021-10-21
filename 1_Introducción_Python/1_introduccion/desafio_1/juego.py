f23515a3b26e209ef9c905893e0c340bc22de99b
santiagofh <santiagofariash@gmail.com>
2021-10-21 15:36:45 -0300

    correción juego

--- 1_Introducción_Python/1_introduccion/desafio_1/juego.py
+++ 1_Introducción_Python/1_introduccion/desafio_1/juego.py
@@ -14,28 +14,30 @@ elif seleccion == "tijera":
     print("usuario juega tijera")
 else:
     juego1="error"
-
-juego2=random.randint(1,3)
-if juego2 == 1:
-    print("computador juega papel")
-elif juego2 == 2:
-    print("computador juega piedra")
-elif juego2 == 3:
-    print("computador juega tijera")
-
-if juego1 == juego2: 
-    print("empate")
-elif juego1 == 1 and juego2 == 2: 
-    print("usuario gana")
-elif juego1 == 1 and juego2 == 3: 
-    print("computador gana")
-elif juego1 == 2 and juego2 == 1: 
-    print("computador gana")
-elif juego1 == 2 and juego2 == 3: 
-    print("usuario gana")
-elif juego1 == 3 and juego2 == 1: 
-    print("usuario gana")
-elif juego1 == 3 and juego2 == 2: 
-    print("computador gana")
-else:
+if juego1=="error":
     print("juegue denuevo")
+else:
+    juego2=random.randint(1,3)
+    if juego2 == 1:
+        print("computador juega papel")
+    elif juego2 == 2:
+        print("computador juega piedra")
+    elif juego2 == 3:
+        print("computador juega tijera")
+        
+    if juego1 == juego2: 
+        print("empate")
+    elif juego1 == 1 and juego2 == 2: 
+        print("usuario gana")
+    elif juego1 == 1 and juego2 == 3: 
+        print("computador gana")
+    elif juego1 == 2 and juego2 == 1: 
+        print("computador gana")
+    elif juego1 == 2 and juego2 == 3: 
+        print("usuario gana")
+    elif juego1 == 3 and juego2 == 1: 
+        print("usuario gana")
+    elif juego1 == 3 and juego2 == 2: 
+        print("computador gana")
+    else:
+        print("juegue denuevo")
