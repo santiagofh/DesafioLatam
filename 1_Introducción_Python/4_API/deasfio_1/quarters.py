f664d2d3c531e83cad5d8fed11c05489830efc76
santiagofh <santiagofariash@gmail.com>
2021-11-09 15:14:40 -0300

    api

---
+++ 1_Introducción_Python/4_API/deasfio_1/quarters.py
@@ -0,0 +1,10 @@
+#%%
+from iter1 import ventas
+from itertools import groupby
+# %%
+ls_q1=["Enero","Febrero","Marzo"]
+ls_q2=["Abril","Mayo","Junio"]
+ls_q3=["Julio","Agosto","Septiembre"]
+ls_q4=["Octubre","Noviembre","Diciembre"]
+
+# %%
